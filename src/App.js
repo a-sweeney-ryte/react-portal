ba633ad39663743aad46d892ddf21f9a73a868cb
Alex Sweeney <a.sweeney@ryte.com>
2022-04-13 18:44:51 +0100

    added simple example

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
+import { useState } from 'react';
+import * as ReactDOM from 'react-dom';
 import './App.css';
 
 function App() {
-  return (
+  const [open, setOpen] = useState(false);
 
+  return (
+    <div className='component'>
+      <button onClick={() => setOpen(true)}>Open Modal</button>
+
+      <Modal isOpen={open} onClose={() => setOpen(false)}>
+        Fancy Modal
+      </Modal>
+    </div>
   );
+}
+
+function Modal({ isOpen, onClose, children }) {
+  if(!isOpen) return null;
+
+  return ReactDOM.createPortal(
+    <div className='modal'>
+      <button onClick={onClose}>Close</button>
+      {children}
+    </div>,
+    document.body
+  )
 }
 
 export default App;
